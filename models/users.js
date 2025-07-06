eef7dfa8e0c2e0e41ac6c57145b00f566f016b1f
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-06 23:35:12 +0530

    created schema

---
+++ models/users.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+    fullNmae:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    state:{
+        type: String,
+        default: "",
+    },
+    city:{
+        type: String,
+        default: "",
+    },
+    locality:{
+        type: String,
+        default: "",
+    },
+    password:{
+        type: String,
+        required: true,
+        trim: true
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
