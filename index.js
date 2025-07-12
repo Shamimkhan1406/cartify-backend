910a8a3bb7e86835e20cfe3fedfc1663e93ea958
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-12 19:49:18 +0530

    rating and review post api endpoint

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const authRouter = require("./routes/auth");
 const bannerRouter = require("./routes/banner");
 const categoryRouter = require("./routes/category");
 const subCategoryRouter = require("./routes/sub_category");
+const productReviewRouter = require("./routes/product_review");
 const productRouter = require("./routes/product");
 // import the dotenv module to load environment variables
 require ("dotenv").config();
@@ -26,8 +27,8 @@ app.use(authRouter);
 app.use(bannerRouter);
 app.use(categoryRouter);
 app.use(subCategoryRouter);
+app.use(productReviewRouter);
 app.use(productRouter);
-
 
 mongoose.connect(DB).then(()=>{
     console.log("connected to database");
