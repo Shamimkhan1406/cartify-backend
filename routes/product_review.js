070b2d0c7680248f2d07973a064e31ccb737ad9b
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-12 19:55:08 +0530

    get request api for review rating

--- routes/product_review.js
+++ routes/product_review.js
@@ -6,8 +6,8 @@ const productReviewRouter = express.Router();
 productReviewRouter.post('/api/product-review', async (req, res) => {
     try {
         {
-            const {buyerId, email, fullName, productId, rating, review} = req.body;
-            const reviews = new ProductReview({buyerId, email, fullName, productId, rating, review});
+            const { buyerId, email, fullName, productId, rating, review } = req.body;
+            const reviews = new ProductReview({ buyerId, email, fullName, productId, rating, review });
             await reviews.save();
             return res.status(201).send(reviews);
         }
@@ -17,5 +17,16 @@ productReviewRouter.post('/api/product-review', async (req, res) => {
         });
     }
 });
+
+productReviewRouter.get('/api/reviews', async (req, res) => {
+    try {
+        const reviews = await ProductReview.find();
+        res.status(200).json({reviews});
+    } catch (e) {
+        res.status(500).json({
+            error: e.message,
+        });
+    }
+})
 
 module.exports = productReviewRouter;
