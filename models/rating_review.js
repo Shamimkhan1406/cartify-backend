da312fd9fd33498d3df39bc9b75e7bc6154ab082
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-12 19:34:31 +0530

    rating and review schema

---
+++ models/rating_review.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const ratingReviewSchema = mongoose.Schema({
+    buyerId: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    productId: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5,
+    },
+    review: {
+        type: String,
+        required: true,
+    },
+});
+
+const ProductReview = mongoose.model('ProductReview', ratingReviewSchema);
+module.exports = ProductReview;
