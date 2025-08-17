50cecb478b9250e53983e625a65a5ffb59feb153
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-08-17 15:50:39 +0530

    created order api

---
+++ routes/order.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const orderRouter = express.Router();
+const Order = require('../models/order');
+
+// post route for creating a new order
+orderRouter.post('/api/orders', async (req, res) => {
+    try {
+        const { fullName, email, state, city, locality, productName, productPrice, quantity, category, image, buyerId, vendorId, } = req.body;
+        const createdAt = new Date.getmilliseconds();
+        // create a new order instance with the extracted data
+        const order = new Order(
+            { fullName, email, state, city, locality, productName, productPrice, quantity, category, image, buyerId, vendorId, createdAt }
+        );
+        // save the order to the database
+        await order.save();
+        // send a success response
+        return res.status(201).json(order);
+    } catch (e) {
+        // handle any errors that occur during the process
+        res.status(500).json({
+            error: e.message,
+        });
+    }
+});
