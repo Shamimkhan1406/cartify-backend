34015ea8115d8a76626a0311836820abf785d177
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-09-07 20:53:53 +0530

    calculate avg rating and total rating

--- routes/product_review.js
+++ routes/product_review.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ProductReview = require('../models/product_review');
 const productReviewRouter = express.Router();
+const Product = require('../models/product');
 
 // Create a new product review
 productReviewRouter.post('/api/product-review', async (req, res) => {
@@ -9,6 +10,19 @@ productReviewRouter.post('/api/product-review', async (req, res) => {
             const { buyerId, email, fullName, productId, rating, review } = req.body;
             const reviews = new ProductReview({ buyerId, email, fullName, productId, rating, review });
             await reviews.save();
+            // find the product associeated with the review by product id
+            const product = await Product.findById(productId);
+            // if product is not found return a 404 status
+            if (!product) {
+                return res.status(404).json({
+                    error: "Product not found",
+                });
+            }
+            // if product is found, update the reviews array with the new review and increment the review count by 1
+            product.totalRating += 1;
+            product.avgRating = (product.avgRating * (product.totalRating - 1) + rating) / product.totalRating;
+            await product.save();
+            
             return res.status(201).send(reviews);
         }
     } catch (e) {
