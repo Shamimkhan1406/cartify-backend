7d6b16d16aa10ff62043f07ab9f79fd44d158aa2
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-09-08 13:37:41 +0530

    privent duplicate reviews and ratings

--- routes/product_review.js
+++ routes/product_review.js
@@ -8,6 +8,13 @@ productReviewRouter.post('/api/product-review', async (req, res) => {
     try {
         {
             const { buyerId, email, fullName, productId, rating, review } = req.body;
+            const existingReview = await ProductReview.findOne({ buyerId, productId });
+            if (existingReview) {
+                return res.status(400).json({
+                    error: "You have already reviewed this product",
+
+                })
+            }
             const reviews = new ProductReview({ buyerId, email, fullName, productId, rating, review });
             await reviews.save();
             // find the product associeated with the review by product id
@@ -22,7 +29,7 @@ productReviewRouter.post('/api/product-review', async (req, res) => {
             product.totalRating += 1;
             product.avgRating = (product.avgRating * (product.totalRating - 1) + rating) / product.totalRating;
             await product.save();
-            
+
             return res.status(201).send(reviews);
         }
     } catch (e) {
@@ -35,7 +42,7 @@ productReviewRouter.post('/api/product-review', async (req, res) => {
 productReviewRouter.get('/api/reviews', async (req, res) => {
     try {
         const reviews = await ProductReview.find();
-        res.status(200).json({reviews});
+        res.status(200).json({ reviews });
     } catch (e) {
         res.status(500).json({
             error: e.message,
