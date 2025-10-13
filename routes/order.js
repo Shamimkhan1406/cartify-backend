c5ad941d5e0cc5525ccacbbdcd3345615496ea42
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-10-13 14:57:16 +0530

    payment processing api

--- routes/order.js
+++ routes/order.js
@@ -1,7 +1,56 @@
 const express = require('express');
 const orderRouter = express.Router();
 const Order = require('../models/order');
+// import the dotenv module to load environment variables
+require ("dotenv").config();
+const stripe = require('stripe')(process.env.stripe_key);
 const { auth, vendorAuth } = require("../middleware/auth");
+const { status } = require('express/lib/response');
+
+// create a payment intent
+orderRouter.post('/api/payment', async (req, res)=>{
+    try {
+        const { orderId,paymentMethodId, currency='inr'} = req.body;
+        // validate the pressence of the required fields
+        if (!orderId || !paymentMethodId || !currency){
+            return res.status(400).json({
+                msg:"All fields are required"
+            });
+        }
+        // query for the order by orderId
+        const order = await Order.findById(orderId);
+        if (!order){
+            console.log("Order not found");
+            return res.status(404).json({
+                msg:"Order not found"
+            });
+        }
+        // calculate the total amount (price * quantity)
+        const amount = order.productPrice * order.quantity * 100; // convert to the smallest currency unit
+        // create a payment intent with the specified amount, currency, and payment method
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: currency,
+            payment_method: paymentMethodId,
+            automatic_payment_methods: {
+                enabled: true,
+            },
+            //confirm: true, // confirm the payment immediately
+        });
+        console.log("payment status: ",paymentIntent.status);
+        return res.json({
+            status: 'success',
+            paymentIntentId: paymentIntent.id,
+            amount: paymentIntent.amount / 100, // convert back to the main currency unit
+            currency: paymentIntent.currency,
+            
+        })
+    } catch (e) {
+        res.status(500).json({
+            error: e.message,
+        });
+    }
+})
 
 // post route for creating a new order
 orderRouter.post('/api/orders', auth, async (req, res) => {
